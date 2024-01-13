dd3016788c526acc0ba92f7bec94f6a91233b1fd
somervda <davidsomerville@comcast.net>
2024-01-13 11:49:20 -0500

    Test

--- noteToMidi.py
+++ noteToMidi.py
@@ -1,6 +1,8 @@
 NOTES_FLAT = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 NOTES_SHARP = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
+#
+
 def NoteToMidi(KeyOctave):
     # KeyOctave is formatted like 'C#3'
     key = KeyOctave[:-1]  # eg C, Db
