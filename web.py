2c36d10f6c2c81438789152ff79182c0f038ab5b
pimidi <davidsomerville@comcast.net>
2024-01-31 10:20:35 -0500

    Improved version of pimidi.service correctlt waits for the network service to come online

--- web.py
+++ web.py
@@ -26,13 +26,41 @@ def midi_note_on(note: Annotated[int, Path(title="Midi note value",le=127)]):
     o.noteOn(note)
     return{True}
 
+@app.get("/midiNoteOnC/{note}/{channel}")
+def midi_note_on_C(note: Annotated[int, Path(title="Midi note value",le=127)],
+    channel: Annotated[int, Path(title="Midi channel",ge=1,le=16)]):
+    o.noteOn(note,channel=channel)
+    return{True}
+
+@app.get("/midiNoteOnCV/{note}/{channel}/{velocity}")
+def midi_note_on_CV(note: Annotated[int, Path(title="Midi note value",le=127)],
+channel: Annotated[int, Path(title="Midi channel",ge=1,le=16)],
+    velocity: Annotated[int, Path(title="Note velocity",le=127)]):
+    o.noteOn(note,velocity=velocity,channel=channel)
+    return{True}
+
+
 @app.get("/midiNoteOff/{note}")
 def midi_note_off(note: Annotated[int, Path(title="Midi note value",le=127)]):
     o.noteOff(note)
     return{True}
 
+@app.get("/midiNoteOffC/{note}/{channel}")
+def midi_note_off_C(note: Annotated[int, Path(title="Midi note value",le=127)],
+    channel: Annotated[int, Path(title="Midi channel",ge=1,le=16)]):
+    o.noteOff(note,channel=channel)
+    return{True}
+
+@app.get("/midiNoteOffCV/{note}/{channel}/{velocity}")
+def midi_note_off_CV(note: Annotated[int, Path(title="Midi note value",le=127)],
+    channel: Annotated[int, Path(title="Midi channel",ge=1,le=16)],
+    velocity: Annotated[int, Path(title="Note velocity",le=127)]):
+    o.noteOff(note,channel=channel,velocity=velocity)
+    return{True}
+
 @app.get("/midiNoteReset")
 def midi_note_reset():
+    # Reset any active midi notes (incase of a clitch and something got held on)
     for n in range(1,127):
         o.noteOff(n)
     return{True}
