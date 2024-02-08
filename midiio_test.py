c1313c7d7a1ecc0665a12a7769d08469b59c9fa3
pizero2w <davidsomerville@comcast.net>
2024-02-08 12:53:14 -0500

    Better tests

--- midiio_test.py
+++ midiio_test.py
@@ -5,26 +5,29 @@ import asyncio
 
 from midiio import MidiIO
 
+o = MidiIO()
+
 async def main():
-    o = MidiIO()
+    print("start noteplay")
+    # task = asyncio.create_task(o.notePlay(44,1))
+    # print("end noteplay")
+    # await task
+    # print("finish main")
 
-    print("start")
-    task = asyncio.create_task(o.notePlay(44,1))
-    print("end")
-    await task
-    print("finish")
+print("* Basic getter, setter tests *")
+o.cv_max_volts=o.cv_max_volts
+print(o.cv_max_volts)
+o.cv_midi_channel=o.cv_midi_channel
+print(o.cv_midi_channel)
+o.cv_min_hertz=o.cv_min_hertz
+print(o.cv_min_hertz)
+o.midi_display=o.midi_display
+print(o.midi_display)
+o.midi_default_channel=o.midi_default_channel
+print(o.midi_default_channel)
+o.settingsSave()
 
-
+print("* Test asyncronous functions")
 asyncio.run(main())
 
-# print(o.cv_max_volts)
-# o.cv_max_volts=4.5
-# print(o.cv_midi_channel)
-# o.cv_midi_channel=2
-# print(o.cv_min_hertz)
-# o.cv_min_hertz=330
-# print(o.midi_display)
-# o.midi_display=False
-# print(o.midi_default_channel)
-# o.midi_default_channel=3
-# o.settingsSave()
+
